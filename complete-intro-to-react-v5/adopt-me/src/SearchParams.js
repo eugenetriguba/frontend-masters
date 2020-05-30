2110dd3b2fb8613b3be3fcb63699d12825948898
Eugene Triguba <eugenetriguba@gmail.com>
2020-05-30 09:40:07 -0500

    Initial Commit

---
+++ complete-intro-to-react-v5/adopt-me/src/SearchParams.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("")
+  const [breeds, setBreeds] = useState([])
+  
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input 
+            type="text" value={location} 
+            placeholder="Location" 
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select 
+            id="animal" 
+            value={animal} 
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+            option
+          >
+              <option>All</option>
+              {ANIMALS.map(animal => (
+                <option value={animal} key={animal}>
+                  {animal}
+                </option>
+              ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select 
+            id="breed"
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+            onBlur={e => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map(breedString => <option key={breedString}>{breedString}</option>)}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+    );
+  };
+  
+  export default SearchParams;
